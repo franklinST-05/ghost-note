72c11ab326a1128d5a62655b9ad979d74c8b11c9
Franklin <franklinteixeira205@gmail.com>
2023-06-18 16:37:35 -0300

    feat: endpoint create note

--- src/pages/api/note/index.ts
+++ src/pages/api/note/index.ts
@@ -1,13 +1,44 @@
+import client from '@/infra/prisma';
 import { HttpResponse } from '@/protocols/http';
 import routerHandler from '@/utils/router-handler';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { createRouter } from 'next-connect';
+import slugify from 'slugify';
+import validator from 'validator';
 
 const router = createRouter<NextApiRequest, NextApiResponse<HttpResponse>>();
 
-router.get((req, res) => {
+router.post(async (req, res) => {
+
+    const { identifier, body } = req.body;
+
+    const validIdentifier = validator.isLength(identifier, { min: 3 })
+    if (!validIdentifier) {
+        return res.status(400).json({
+            error: 'Invalid identifier',
+        });
+    }
+
+    const suggestIdentifier = slugify(identifier, {
+        lower: true,
+        trim: true,
+    });
+
+    if(identifier != suggestIdentifier) {
+        return res.status(400).json({
+            error: 'Invalid identifier',
+            data: {
+                suggest: suggestIdentifier,
+            }
+        });
+    }
+
+    const note = await client.note.create({
+        data: { identifier, body }
+    });
+
     return res.status(400).json({
-        error: 'not implemented'
+        data: { note }
     });
 });
 
