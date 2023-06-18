f2d692cc8332e4ac9811e3496c3728dec3de318e
Franklin <franklinteixeira205@gmail.com>
2023-06-18 18:50:24 -0300

    fix: resolve not found method onChange

--- src/pages/[identifier].tsx
+++ src/pages/[identifier].tsx
@@ -1,9 +1,17 @@
-import Head from 'next/head';
 import { Note } from '@prisma/client';
 import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import { ChangeEvent, useState } from 'react';
 import { FiLink } from 'react-icons/fi';
 
 const EditorPage: React.FC<EditorPageProps> = ({ note }) => {
+
+  const [body, setBody] = useState<string>(note.body ?? '');
+  
+  const onChangeBody = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setBody(e.target.value)
+  }
+
   return (
     <main className="container-app">
       <Head>
@@ -11,18 +19,20 @@ const EditorPage: React.FC<EditorPageProps> = ({ note }) => {
         <meta name="description" content={note.identifier} />
         <meta name="keywords" content={note.identifier} />
       </Head>
-      
+
       <header className="w-full py-8">
         <button className="group flex items-center gap-2">
           <p className="text-base font-medium">{note.identifier}</p>
-          <FiLink className="text-gray-500 group-hover:text-white"/>
+          <FiLink className="text-gray-500 group-hover:text-white" />
         </button>
       </header>
 
       <section>
-        <textarea className="w-full min-h-full bg-transparent outline-none">
-          {note.body}
-        </textarea>
+        <textarea
+          value={body}
+          onChange={onChangeBody}
+          className="w-full min-h-full bg-transparent outline-none"
+        />
       </section>
     </main>
   );
