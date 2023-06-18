c0797fa0ff6b96abdbdc7ffd0f866482602bf8ef
Franklin <franklinteixeira205@gmail.com>
2023-06-18 17:57:08 -0300

    feat: title head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import { AppProps } from 'next/app';
 import './styles/global.css';
+import Head from 'next/head';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <Component {...pageProps} />
+    <>
+      <Head>
+        <title>GhostNote</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
   );
 };
 
